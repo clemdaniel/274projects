5a78f4da15a27931f0d3b3a2750b229ec6941e8d
Daniel Clements <danielclements@peerparking.net>
2015-12-02 19:22:49 -0500

    fix tab width inconsistencies

--- project4/sensors.c
+++ project4/sensors.c
@@ -20,77 +20,77 @@ void findWall(void) {
 // value from 0-4095,
 // 4095 => as close to wall as possible 
 uint16_t getWallDistance(void) {
-    uint8_t hi, lo;
-    byteTx(CmdSensors);
-    byteTx(WallPID);
-    hi = byteRx();
-    lo = byteRx();
-    return (hi << 8) | lo;
+	uint8_t hi, lo;
+	byteTx(CmdSensors);
+	byteTx(WallPID);
+	hi = byteRx();
+	lo = byteRx();
+	return (hi << 8) | lo;
 }
 
 //determines whether bump sensors are active
 void getBumps(void) {
-    uint8_t bumps;
+	uint8_t bumps;
 		byteTx(CmdSensors);
-    byteTx(BumpAndWheeldropPID);
-    bumps = byteRx(); 
-    bumpLeft = bumps & (1 << 1);
-    bumpRight = bumps & (1 << 0);
+	byteTx(BumpAndWheeldropPID);
+	bumps = byteRx(); 
+	bumpLeft = bumps & (1 << 1);
+	bumpRight = bumps & (1 << 0);
 }
 
 //print characters containing in char array 
 //to serial - returns length of string
-//  -useful for debugging
+//	-useful for debugging
 int transmit(char* string) {
-    int i;
-    int length = strlen(string);
-    //point to serial monitor
-    setSerialDestination(SERIAL_USB);
-    //send data to USB
-    for (i=0; i<length; i++) {
-        byteTx(string[i]);
-    }
-    //point back to create
-    setSerialDestination(SERIAL_CREATE);
-    return length;
+	int i;
+	int length = strlen(string);
+	//point to serial monitor
+	setSerialDestination(SERIAL_USB);
+	//send data to USB
+	for (i=0; i<length; i++) {
+		byteTx(string[i]);
+	}
+	//point back to create
+	setSerialDestination(SERIAL_CREATE);
+	return length;
 }
 
 void getDockSenses(void) {
-    byteTx(CmdSensors);
-    byteTx(IRsensorPacket); //packet for IR control
-    irSensor = byteRx();
-    if (irSensor == RED_BUOY) {
-        red = 1;
-        green = 0;
-        forceField = 0;
-    } else if (irSensor == GREEN_BUOY) {
-        red = 0;
-        green = 1;
-        forceField = 0;
-    } else if (irSensor == FORCE_FIELD) {
-        red = 0;
-        green = 0;
-        forceField = 1;
-    } else if (irSensor == R_AND_G) {
-        red = 1;
-        green = 1;
-        forceField = 0;
-    } else if (irSensor == R_AND_F) {
-        red = 1;
-        green = 0;
-        forceField = 1;
-    } else if (irSensor == G_AND_F) {
-        red = 0;
-        green = 1;
-        forceField = 1;
-    } else if (irSensor == ALL_THREE) {
-        red = 1;
-        green = 1;
-        forceField = 1;
-    } else if (irSensor == NO_SIGNAL) {
-        red = 0;
-        green = 0;
-        forceField = 0;
-    }
+	byteTx(CmdSensors);
+	byteTx(IRsensorPacket); //packet for IR control
+	irSensor = byteRx();
+	if (irSensor == RED_BUOY) {
+		red = 1;
+		green = 0;
+		forceField = 0;
+	} else if (irSensor == GREEN_BUOY) {
+		red = 0;
+		green = 1;
+		forceField = 0;
+	} else if (irSensor == FORCE_FIELD) {
+		red = 0;
+		green = 0;
+		forceField = 1;
+	} else if (irSensor == R_AND_G) {
+		red = 1;
+		green = 1;
+		forceField = 0;
+	} else if (irSensor == R_AND_F) {
+		red = 1;
+		green = 0;
+		forceField = 1;
+	} else if (irSensor == G_AND_F) {
+		red = 0;
+		green = 1;
+		forceField = 1;
+	} else if (irSensor == ALL_THREE) {
+		red = 1;
+		green = 1;
+		forceField = 1;
+	} else if (irSensor == NO_SIGNAL) {
+		red = 0;
+		green = 0;
+		forceField = 0;
+	}
 }
 
